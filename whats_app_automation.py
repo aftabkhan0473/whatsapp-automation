8fcfa5251d47a4bc081bf40b5e3d41eab0761d25
Aftab <therealtopper@gmail.com>
2022-11-27 10:26:25 +0530

    push after completion

---
+++ whats_app_automation.py
@@ -0,0 +1,10 @@
+import pywhatkit as kt
+import getpass as gp
+
+print("Let's automate whatsapp!")
+
+# p_num = gp.getpass(prompt='PhoneNumber: ', stream =None)
+
+msg = "Miss you bro!"
+
+kt.sendwhatmsg("+916205377925",msg,8,59)
